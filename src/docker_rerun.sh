b1ada3265ed973664b4cbb5589f052d773c75ed9
Dmitry Monakhov <d.a.monakhov@gmail.com>
2020-02-09 22:58:16 +0300

    Added configuration files for docker-3

--- src/docker_rerun.sh
+++ src/docker_rerun.sh
@@ -1,9 +1,9 @@
-docker build -t docker-267016/comment:2.0 -f ./comment/Dockerfile.2 ./comment
+docker build -t docker-267016/comment:1.0 -f ./comment/Dockerfile ./comment
 docker build -t docker-267016/post:1.0 -f ./post-py/Dockerfile ./post-py
-docker build -t docker-267016/ui:2.0 -f ./ui/Dockerfile.2 ./ui
+docker build -t docker-267016/ui:1.0 -f ./ui/Dockerfile ./ui
 
 docker rm -f $(docker ps -a -q)
 docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db mongo:latest
 docker run -d --network=reddit --network-alias=post docker-267016/post:1.0
-docker run -d --network=reddit --network-alias=comment docker-267016/comment:2.0
-docker run -d --network=reddit -p 9292:9292 docker-267016/ui:2.0
+docker run -d --network=reddit --network-alias=comment docker-267016/comment:1.0
+docker run -d --network=reddit -p 9292:9292 docker-267016/ui:1.0
